ffdbff63bbfda4069c7d117d4fd746bd7a8c11b7
Marcel Kauffmann <3215600+mattoy@users.noreply.github.com>
2026-08-16 09:08:54 +0200

    Added type declarations

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
 
 /// <reference types="vitest" />
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    dts({
+      include: ['src/**/*.ts', 'src/**/*.vue'],
+      outDir: 'dist',
+      rollupTypes: true,
+      entryRoot: 'src/components/Treemap',
+      insertTypesEntry: true
+    })
+  ],
   build: {
     lib: {
       entry: 'src/components/Treemap/index.ts',
